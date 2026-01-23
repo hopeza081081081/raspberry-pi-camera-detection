ca8af002d3de53354fbd5c91c4c097f295d41a9d
Tanakorn Khunkhao <tanakorn100@hotmail.com>
2026-01-24 02:21:23 +0700

    chaneg HEADLESS_MODE to False

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ CONFIDENCE_THRESHOLD = 0.5 # 0.1 to 1.0
 TARGET_LABELS = ["person"] # List of labels to detect
 
 # Performance Settings
-HEADLESS_MODE = True # Set to False if you have a monitor connected and want to see the video
+HEADLESS_MODE = False # Set to False if you have a monitor connected and want to see the video
 NUM_THREADS = 4 # Number of threads for TFLite inference (4 is good for Pi 4)
